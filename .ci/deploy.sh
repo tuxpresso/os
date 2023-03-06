cb7d9ef3bc7b0a220ba400c2a42e5c750e22a825
Andrew Hepp <andrew.hepp@ahepp.dev>
2023-03-05 16:21:36 -0800

    ci: deploy to s3

---
+++ .ci/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+host=$1
+s3_key=$2
+s3_secret=$3
+file=$4
+resource=$5
+
+scheme="https"
+content_type="application/octet-stream"
+date=`date -R`
+_signature="PUT\n\n${content_type}\n${date}\n${resource}"
+signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
+
+curl -X PUT -T "${file}" \
+          -H "Host: ${host}" \
+          -H "Date: ${date}" \
+          -H "Content-Type: ${content_type}" \
+          -H "Authorization: AWS ${s3_key}:${signature}" \
+          ${scheme}://${host}${resource}
